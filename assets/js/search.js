58455180eda80f4a4e8a0149bbc56a61ea9a06b3
David P. <root@enfaseterminal.com>
2025-02-03 21:49:37 +0100

    Una vez mas

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   fetch('https://www.enfaseterminal.com/assets/json/search.json')
     .then(response => response.json())
     .then(data => {
+      console.log("Datos JSON cargados:", data);
       const idx = lunr(function () {
         this.ref('url')
         this.field('title')
@@ -13,18 +14,25 @@ document.addEventListener('DOMContentLoaded', function() {
       })
 
       document.getElementById('search-input').addEventListener('input', function () {
-        const results = idx.search(this.value)
-        const resultsContainer = document.getElementById('results-container')
-        resultsContainer.innerHTML = ''
+        const query = this.value;
+        console.log("Consulta de búsqueda:", query);
+        const results = idx.search(query);
+        console.log("Resultados de búsqueda:", results);
+        const resultsContainer = document.getElementById('results-container');
+        resultsContainer.innerHTML = '';
         results.forEach(result => {
-          const item = data.find(doc => doc.url === result.ref)
-          const li = document.createElement('li')
-          li.innerHTML = `<a href="${item.url}">${item.title}</a>`
-          resultsContainer.appendChild(li)
+          const item = data.find(doc => doc.url === result.ref);
+          const li = document.createElement('li');
+          li.innerHTML = `<a href="${item.url}">${item.title}</a>`;
+          resultsContainer.appendChild(li);
         })
       })
     })
+    .catch(error => {
+      console.error("Error al cargar el archivo JSON:", error);
+    })
 })
+
 
 
 
